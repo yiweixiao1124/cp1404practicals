4c79831ba2b210bbc62366603783a6384c8200af
YiweiXiao <yiwei.xiao@my.jcu.edu.au>
2023-11-18 15:19:51 +0800

    prac02

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -3,11 +3,11 @@ MINI_LENGTH = 8
 
 def main():
     password = input("Enter password: ")
-    get_password(password)
+    check_password(password)
     the_password(password)
 
 
-def get_password(password):
+def check_password(password):
     while len(password) < MINI_LENGTH:
         print("The password length must be at least greater than 8 bits.")
         password = input("Enter password: ")
