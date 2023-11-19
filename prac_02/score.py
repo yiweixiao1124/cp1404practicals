4ed3305f63c1fb3910cc1a3f1ca8957fb904b12e
YiweiXiao <yiwei.xiao@my.jcu.edu.au>
2023-11-19 23:34:51 +0800

    prac02

--- prac_02/score.py
+++ prac_02/score.py
@@ -3,17 +3,23 @@ MAX_SCORE = 100
 EXCELLENT_THRESHOLD = 90
 PASS_THRESHOLD = 50
 
-score = float(input("Enter score: "))
-while score < MIN_SCORE or score > MAX_SCORE:
-    print("Invalid score")
-    score = float(input("Enter score: "))
 
-if score >= EXCELLENT_THRESHOLD:
-    message = "Excellent"
-elif score >= PASS_THRESHOLD:
-    message = "Passable"
-else:
-    message = "Bad"
+def main():
+    score = float(input("请输入分数: "))
+    while score < MIN_SCORE or score > MAX_SCORE:
+        print("无效的分数")
+        score = float(input("请输入分数: "))
+    message = get_score_result(score)
+    print(message)
 
-print(message)
 
+def get_score_result(score):
+    if score >= EXCELLENT_THRESHOLD:
+        return "Excellent"
+    elif score >= PASS_THRESHOLD:
+        return "Passable"
+    else:
+        return "Bad"
+
+
+main()
