6c0f7ce6414cf302d6d031e294b68ea962661147
YiweiXiao <yiwei.xiao@my.jcu.edu.au>
2023-11-19 23:39:42 +0800

    prac

--- prac_02/score.py
+++ prac_02/score.py
@@ -1,3 +1,4 @@
+import random
 MIN_SCORE = 0
 MAX_SCORE = 100
 EXCELLENT_THRESHOLD = 90
@@ -11,6 +12,11 @@ def main():
         score = float(input("Enter score: "))
     message = get_score_result(score)
     print(message)
+
+    random_score = random.randint(MIN_SCORE, MAX_SCORE)
+    print(f"Random score: {random_score}")
+    random_message = get_score_result(random_score)
+    print(random_message)
 
 
 def get_score_result(score):
