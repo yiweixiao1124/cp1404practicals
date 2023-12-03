fc79f97bacdad1e19e904df5229cac26908efe82
YiweiXiao <yiwei.xiao@my.jcu.edu.au>
2023-12-03 19:39:23 +0800

    prac04

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -2,8 +2,17 @@ numbers = []
 for number in range(5):
     number = int(input("Number: "))
     numbers.append(number)
-print(f"The first number is{numbers[0]}")
+print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
-print(f"The smallest number is{min(numbers)}")
-print(f"The largest number is{max(numbers)}")
-print(f"The average number is{sum(numbers) / len(numbers)}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+print(f"The average number is {sum(numbers) / len(numbers)}")
+print()
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Enter username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access Denied")
