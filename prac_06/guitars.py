5c4cd3d36aa7c0a55ecb26f1fadb8b127bb79ce9
YiweiXiao <yiwei.xiao@my.jcu.edu.au>
2023-12-11 15:43:38 +0800

    prac06

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -6,16 +6,15 @@ def main():
     guitars = []
 
     print("My Guitars!")
-    while True:
-        name = input("Name: ")
-        if name == "":
-            break
+    name = input("Name: ")
+    while name != "":
         year = int(input("Year: "))
         cost = float(input("Cost: $"))
         guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
         guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
         guitars.append(Guitar(name, year, cost))
         print(f"{name} ({year}) : ${cost} added")
+        name = input("Name: ")
 
     for i, guitar in enumerate(guitars, 1):
         if guitar.is_vintage():
