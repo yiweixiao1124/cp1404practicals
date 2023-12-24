d8a19ac057fdbd8eda3c265055ba20f2999de5cf
YiweiXiao <yiwei.xiao@my.jcu.edu.au>
2023-12-24 21:53:57 +0800

    prac 08

---
+++ prac_08/dynamic_labels.py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+
+
+class DynamicLabelsApp(App):
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        self.names = {"Bob Brown", "Cat Cyan", "Oren Ochre"}
+
+    def build(self):
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_labels()
+        return self.root
+
+    def create_labels(self):
+        for name in self.names:
+            temp_label = Label(text=name)
+            self.root.ids.main.add_widget(temp_label)
+
+
+DynamicLabelsApp().run()
