54d712931d661471cc02888c5827a62e9771dfcd
YiweiXiao <yiwei.xiao@my.jcu.edu.au>
2024-01-17 16:12:14 +0800

    modified prac07

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -1,5 +1,7 @@
 CURRENT_YEAR = 2023
 VINTAGE_YEAR = 50
+
+
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
         self.name = name
--- prac_07/language_file_reader.py
+++ prac_07/language_file_reader.py
@@ -64,7 +64,7 @@ def using_namedtuple():
     file_field_names = in_file.readline().strip().split(',')
     print(file_field_names)
     # Language will be a new subclass of the tuple data type class
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, year, pointer_arithmetic')
     reader = csv.reader(in_file)  # use default dialect, Excel
 
     for row in reader:
@@ -79,7 +79,7 @@ def using_namedtuple():
 
 def using_csv_namedtuple():
     """Language file reader version using both csv module and named tuple."""
-    Language = namedtuple('Language', 'name, typing, reflection, year')
+    Language = namedtuple('Language', 'name, typing, reflection, year, pointer_arithmetic')
     in_file = open("languages.csv", "r")
     in_file.readline()
     for language in map(Language._make, csv.reader(in_file)):
