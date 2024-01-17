0e60a92de46403713bf587c74773d8c2510c5c91
YiweiXiao <yiwei.xiao@my.jcu.edu.au>
2024-01-17 21:51:47 +0800

    modified prac08

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,33 +1,37 @@
 from kivy.app import App
 from kivy.lang import Builder
-
+from kivy.properties import StringProperty
 
 MILES_TO_KM = 1.60934
 
 
 class MilesConvertApp(App):
+    output_km = StringProperty()
 
     def build(self):
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
-    def handle_calculate(self):
-        value = self.get_valid_miles()
-        result = value * MILES_TO_KM
-        self.root.ids.output_label.text = str(result)
+    def handle_calculate(self, text):
+        print("handle calculate")
+        miles = self.convert_to_number(text)
+        self.update_result(float(miles))
 
-    def get_valid_miles(self):
+    def handle_increment(self, text, change):
+        print("handle increment")
+        miles = self.concert_to_number(text) + change
+        self.root.ids.input_miles.text = str(miles)
+
+    def update_result(self, miles):
+        print("update")
+        self.output_km = str(miles * MILES_TO_KM)
+
+    @staticmethod
+    def convert_to_number(text):
         try:
-            value = float(self.root.ids.input_miles.text)
-            return value
+            return float(text)
         except ValueError:
             return 0.0
-
-    def handle_increment(self, change):
-        value = self.get_valid_miles() + change
-        self.root.ids.input_miles.text = str(value)
-        self.handle_calculate()
-
 
 MilesConvertApp().run()
--- prac_08/dynamic_labels.py
+++ prac_08/dynamic_labels.py
@@ -8,18 +8,22 @@ class DynamicLabelsApp(App):
     def __init__(self, **kwargs):
         """Construct main app."""
         super().__init__(**kwargs)
-        self.names = {"Bob Brown", "Cat Cyan", "Oren Ochre"}
+        self.names = {"Bob Brown": "021565", "Cat Cyan": "549644", "Oren Ochre": "024685"}
 
     def build(self):
+        """Build the Kivy GUI"""
+
         self.title = "Dynamic Labels"
         self.root = Builder.load_file('dynamic_labels.kv')
         self.create_labels()
         return self.root
 
     def create_labels(self):
-        for name in self.names:
+        """Creotes label from dictionary"""
+        for name in self.name_to_phone:
             temp_label = Label(text=name)
-            self.root.ids.main.add_widget(temp_label)
+            temp_label.color = (1, 0, 0, 1)
+            self.root.ids.label_box.add_widget(temp_label)
 
 
 DynamicLabelsApp().run()
